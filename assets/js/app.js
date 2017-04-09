b37053f6296b579c72642ea6eebd678c2bac8491
India Evans <indiaaeevans@gmail.com>
2017-04-09 16:31:59 -0400

    working app

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-// Initialize Firebas
+// Initialize Firebase
 var config = {
     apiKey: "AIzaSyBZxeldNDs0JLVabu-7gMpOFb1mpSVK2GM",
     authDomain: "train-app-17ee9.firebaseapp.com",
@@ -12,17 +12,18 @@ var config = {
   var database = firebase.database();
   var tableBody = $("#trainTable > tbody");
 
-// BUTTON CLICK EVENT FOR ADDING NEW TRAIN TO THE TABLE
+// BUTTON CLICK EVENT FOR ADDING NEW TRAIN TO THE DATABASE
 $("#submit").on("click", function(){
 	// prevent page reload when button is clicked
     event.preventDefault();
 
-var inputName = $("#inputName").val().trim(),
-	inputDest = $("#inputDest").val().trim(),
-	inputFirst = $("#inputFirst").val().trim(),
-	inputFreq = $("#inputFreq").val().trim();
+    // store user input
+	var inputName = $("#inputName").val().trim(),
+		inputDest = $("#inputDest").val().trim(),
+		inputFirst = $("#inputFirst").val().trim(),
+		inputFreq = $("#inputFreq").val().trim();
 
-// Creates local "temporary" object for holding train data
+	// Creates local "temporary" object for holding train data
   	var newTrain = {
     	name: inputName,
     	dest: inputDest,
@@ -33,7 +34,7 @@ var inputName = $("#inputName").val().trim(),
 	 // Uploads train data to the database
 	 database.ref().push(newTrain);
 
-	 // Clears all of the text-boxes
+	 // Clears the form
 	 $("#inputName").val("");
 	 $("#inputDest").val("");
 	 $("#inputFirst").val("");
@@ -43,11 +44,8 @@ var inputName = $("#inputName").val().trim(),
 	 return false;
 });
 
-// 3. Create Firebase event for adding train to the database
-// and a row in the html when a user adds an entry
+// 3. Create Firebase event for adding train to the table
 database.ref().on("child_added", function(childSnapshot, prevChildKey) {
-
-  	// console.log(childSnapshot.val());
 
 	 // Store everything into a variable.
 	 var name = childSnapshot.val().name;
@@ -59,30 +57,26 @@ database.ref().on("child_added", function(childSnapshot, prevChildKey) {
 
      // First Time (pushed back 1 year to make sure it comes before current time)
     var firstTimeConverted = moment(first, "hh:mm").subtract(1, "years");
-    // console.log(firstTimeConverted);
 
     // Current Time
     var currentTime = moment();
-    // console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
 
     // Difference between the times
     var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-    // console.log("DIFFERENCE IN TIME: " + diffTime);
 
     // Time apart (remainder)
     var tRemainder = diffTime % frequency;
-    // console.log(tRemainder);
 
     // Minutes Until Next Train
     var minutesAway = frequency - tRemainder;
-    // console.log("MINUTES TILL TRAIN: " + minutesAway);
 
     // Next Train
     var nextArrival = moment().add(minutesAway, "minutes");
-    // console.log("ARRIVAL TIME: " + moment(nextArrival).format("hh:mm"));
 
     var arrivalFormatted = moment(nextArrival).format("hh:mm");
 
-	tableBody.append("<tr><td>" + name + "</td><td>" + destination + "</td><td>" + frequency + 
+    // display our data in the table - each train will take up a table row
+	tableBody.append("<tr><td contentEditable>" + name + "</td><td>" + destination + "</td><td>" + frequency + 
 	"</td><td>" + arrivalFormatted + "</td><td>" + minutesAway + "</td></tr>");
   });
+
